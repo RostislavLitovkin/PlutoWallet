cd9ed529bcbdb9f31424edd419f2a39b4d614cd4
Dušan Jánsky <cisar2218@gmail.com>
2023-02-01 23:20:01 +0100

    Plutonication init
    - solution (.net maui class library) added to project
    - created and tested methods that:
    1. opens endpoint at local host and starts listening at specified port
    2. connects to (1.) via local host
    - note: no data transfer yet

--- PlutoWallet/ViewModel/MainViewModel.cs
+++ PlutoWallet/ViewModel/MainViewModel.cs
@@ -1,12 +1,16 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Plutonication;
 using PlutoWallet.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace PlutoWallet.ViewModel
 {
@@ -18,12 +22,40 @@ namespace PlutoWallet.ViewModel
         [RelayCommand]
         private void IncrementCounter()
         {
-            Response = NetworkingModel.RequestSample();
+            TestConnect();
         }
 
         public MainViewModel()
         {
-            response = "request me ^^";
+            TestListen();
+        }
+
+        private void TestConnect()
+        {
+            IPAddress ipAddr = IPAddress.Parse("127.0.0.1");
+            int port = 8080;
+            Socket soc = ConnectionManager.Connect(ipAddr, port);
+            if (soc != null)
+            {
+                Response = "socket opened";
+            }
+            else
+            {
+                Response = "socket failed to open :(";
+            }
+        }
+        private void TestListen()
+        {
+            int port = 8080;
+            Socket soc = ConnectionManager.Listen(port);
+            if (soc != null)
+            {
+                Response = "socket listening";
+            }
+            else
+            {
+                Response = "socket failed to listen :(";
+            }
         }
     }
 }
---
+++ Plutonication/ConnectionManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Plutonication
+{
+    // All the code in this file is included in all platforms.
+    public class ConnectionManager
+    {
+        public static Socket Connect(IPAddress address, int port)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint endpoint = new IPEndPoint(address, port);
+
+            try
+            {
+                socket.Connect(endpoint);
+                return socket;
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine("Failed to connect: " + ex.Message);
+                return null;
+            }
+        }
+        public static Socket Listen(int port)
+        {
+            IPAddress ipLocal = IPAddress.Any;
+            string ipStr = ipLocal.ToString();
+            IPEndPoint localEndPoint = new IPEndPoint(ipLocal, port);
+
+            Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            listener.Bind(localEndPoint);
+            listener.Listen();
+
+            return listener;
+        }
+    }
+}
