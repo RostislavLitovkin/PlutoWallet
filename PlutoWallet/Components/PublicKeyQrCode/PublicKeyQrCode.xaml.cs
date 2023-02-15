fdb9b6675a4492f2a9cc3227138115d457c30c8d
Martinezada <marinezada@gmail.com>
2023-02-15 21:10:19 +0100

    Qr kod ikonka

---
+++ PlutoWallet/Components/PublicKeyQrCode/PublicKeyQrCode.xaml.cs
@@ -0,0 +1,16 @@
+﻿using ZXing.Net.Maui.Controls;
+
+
+namespace PlutoWallet.Components.PublicKeyQrCode;
+
+public partial class PublicKeyQrCode : ContentView
+{
+	public PublicKeyQrCode()
+	{
+		InitializeComponent();
+
+        PublicKeyQrCode publicKeyQrCode = DependencyService.Get<PublicKeyQrCode>();
+        BindingContext = publicKeyQrCode;
+
+    }
+}
