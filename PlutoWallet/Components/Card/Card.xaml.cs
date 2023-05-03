565fc5f495536603f4fcd9dc7c0c6177b3b5d894
rostislavlitovkin <rostislavlitovkin@gmail.com>
2023-05-03 22:22:33 +0200

    Frame to Border

---
+++ PlutoWallet/Components/Card/Card.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace PlutoWallet.Components.Card;
+
+public partial class Card : ContentView
+{
+	public Card()
+	{
+		InitializeComponent();
+	}
+
+	public Microsoft.Maui.Controls.View View { set { contentView.Content = value; } }
+
+	public bool IsPopup { set { border.Padding = new Thickness(10); } }
+}
--- PlutoWallet/View/NftPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PlutoWallet.View;
-
-public partial class NftPage : ContentPage
-{
-	public NftPage()
-	{
-		InitializeComponent();
-	}
-}
