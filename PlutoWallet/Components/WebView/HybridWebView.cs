ea866efdac6fda5fe36b06681801700bd5d7def4
rostislavlitovkin <rostislavlitovkin@gmail.com>
2023-05-14 19:53:32 +0200

    hybrid view (incomplete)

---
+++ PlutoWallet/Components/WebView/HybridWebView.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PlutoWallet.Components.WebView
+{
+    public class HybridWebView : Microsoft.Maui.Controls.WebView
+    {
+        Action<string> action;
+
+        public static readonly BindableProperty UriProperty = BindableProperty.Create(
+            propertyName: "Uri",
+            returnType: typeof(string),
+            declaringType: typeof(HybridWebView),
+            defaultValue: default(string));
+
+        public string Uri
+        {
+            get { return (string)GetValue(UriProperty); }
+            set { SetValue(UriProperty, value); }
+        }
+
+        public void RegisterAction(Action<string> callback)
+        {
+            action = callback;
+        }
+
+        public void Cleanup()
+        {
+            action = null;
+        }
+
+        public void InvokeAction(string data)
+        {
+            if (action == null || data == null)
+            {
+                return;
+            }
+            action.Invoke(data);
+        }
+    }
+}
+
---
+++ PlutoWallet/Components/WebView/HybridWebViewRenderer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.Maui.Controls.Compatibility;
+using PlutoWallet.Components.WebView;
+using Microsoft.Maui.Controls.Platform;
+
+
+
+#if IOS || MACCATALYST
+using WebKit;
+using CoreGraphics;
+using Microsoft.Maui.Platform;
+using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Controls.Compatibility.Platform.iOS;
+using Foundation;
+
+//[assembly: ExportRenderer(typeof(HybridWebView), typeof(HybridWebViewRenderer))]
+namespace PlutoWallet.Components.WebView
+{
+    public class HybridWebViewRenderer : WkWebViewRenderer, IWKScriptMessageHandler
+    {
+        const string JavaScriptFunction = "function injectExtension(data){window.webkit.messageHandlers.invokeAction.postMessage(data);}";
+        WKUserContentController userController;
+
+        public HybridWebViewRenderer() : this(new WKWebViewConfiguration())
+        {
+        }
+
+        public HybridWebViewRenderer(WKWebViewConfiguration config) : base(config)
+        {
+            userController = config.UserContentController;
+            var script = new WKUserScript(new NSString(JavaScriptFunction), WKUserScriptInjectionTime.AtDocumentEnd, false);
+            userController.AddUserScript(script);
+            userController.AddScriptMessageHandler(this, "invokeAction");
+        }
+
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null)
+            {
+                userController.RemoveAllUserScripts();
+                userController.RemoveScriptMessageHandler("invokeAction");
+                HybridWebView hybridWebView = e.OldElement as HybridWebView;
+                hybridWebView.Cleanup();
+            }
+
+            if (e.NewElement != null)
+            {
+                string filename = Path.Combine(NSBundle.MainBundle.BundlePath, $"Content/{((HybridWebView)Element).Uri}");
+                LoadRequest(new NSUrlRequest(new NSUrl(filename, false)));
+            }
+        }
+
+        public void DidReceiveScriptMessage(WKUserContentController userContentController, WKScriptMessage message)
+        {
+            ((HybridWebView)Element).InvokeAction(message.Body.ToString());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                ((HybridWebView)Element).Cleanup();
+            }
+            base.Dispose(disposing);
+        }
+
+
+    }
+}
+#endif
+
