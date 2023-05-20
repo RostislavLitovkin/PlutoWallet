a3d0b22b39702a351cd415983eace2f552768350
rostislavlitovkin <rostislavlitovkin@gmail.com>
2023-05-20 17:33:50 +0200

    authentication base

---
+++ PlutoWallet/View/AuthenticationPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace PlutoWallet.View;
+
+public partial class AuthenticationPage : ContentPage
+{
+	public AuthenticationPage()
+	{
+		InitializeComponent();
+	}
+
+    async void OnAuthenticateClicked(System.Object sender, System.EventArgs e)
+    {
+        try
+        {
+            WebAuthenticatorResult authResult = await WebAuthenticator.Default.AuthenticateAsync(
+                new Uri("https://mysite.com/mobileauth/Microsoft"),
+                new Uri("myapp://"));
+
+            string accessToken = authResult?.AccessToken;
+
+            valueLabel.Text = "Value: " + accessToken;
+            // Do something with the token
+        }
+        catch (TaskCanceledException ex)
+        {
+            // Use stopped auth
+        }
+    }
+}
