f5de625589b4bae79fdb95f3eb5662fd63ac4c1e
rostislavlitovkin <rostislavlitovkin@gmail.com>
2023-10-14 10:44:49 +0200

    HydraDX Predefined layout

---
+++ PlutoWallet/Components/PredefinedLayouts/HydraDXOmnipoolLayoutItem.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace PlutoWallet.Components.PredefinedLayouts;
+
+public partial class HydraDXOmnipoolLayoutItem : ContentView
+{
+    private const string LAYOUT = "plutolayout: [dApp, ExSL, UsdB, ChaK, HDXOmniLiquidity, HDXDCA];[21]";
+
+    public HydraDXOmnipoolLayoutItem()
+	{
+		InitializeComponent();
+	}
+
+    async void OnClicked(System.Object sender, Microsoft.Maui.Controls.TappedEventArgs e)
+    {
+        Model.CustomLayoutModel.SaveLayout(LAYOUT);
+
+        await Navigation.PopAsync();
+    }
+}
