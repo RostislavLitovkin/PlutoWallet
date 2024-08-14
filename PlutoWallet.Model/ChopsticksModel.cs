85d8d8d8977ff681e2408d291b62fa22e003fd08
Jaromir Prochazka <jaromir.prochazka03@gmail.com>
2024-08-14 12:35:48 +0200

    Add Chopstics api and it's test

---
+++ PlutoWallet.Model/ChopsticksModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace PlutoWallet.Model
+{
+    public class ChopsticksModel
+    {
+        public static async Task<string> PostAsync()
+        {
+            var httpClient = new HttpClient();
+
+            using StringContent jsonContent = new(
+                JsonSerializer.Serialize(new
+                {
+                    endpoint = "wss://acala-rpc-2.aca-api.network/ws",
+                    extrinsic = "0x1234"
+                }),
+                Encoding.UTF8,
+                "application/json");
+
+            using HttpResponseMessage response = await httpClient.PostAsync(
+                "http://localhost:8000",
+                jsonContent);
+    
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            Console.WriteLine($"{jsonResponse}\n");
+
+            return jsonResponse;
+
+            // Expected output:
+            //   POST https://jsonplaceholder.typicode.com/todos HTTP/1.1
+            //   {
+            //     "userId": 77,
+            //     "id": 201,
+        }
+
+    }
+}
---
+++ PlutoWalletTests/ChopsticksTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PlutoWallet.Model;
+
+namespace PlutoWalletTests
+{
+    internal class ChopsticksTests
+    {
+        [Test]
+        public async Task ConnectionTestAsync()
+        {
+            await ChopsticksModel.PostAsync();
+        }
+    }
+}
